a1c507b7fd206daa4e80971e6089275881edf1f7
Eric Normann <eric.n@me.com>
2021-05-08 00:16:27 -0400

    add stripe payment

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from 'react';
+import { useLazyQuery } from '@apollo/react-hooks';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import { idbPromise } from '../../utils/helpers'
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx')
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT)
 
     useEffect(()=> {
         async function getCart() {
@@ -23,6 +29,7 @@ const Cart = () => {
         dispatch({ type: TOGGLE_CART })
     }
 
+
     function calculateTotal() {
         let sum = 0;
         state.cart.forEach(item => {
@@ -30,6 +37,29 @@ const Cart = () => {
         });
        return sum.toFixed(2);
     }
+
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+        
+        getCheckout({
+            variables: { products: productIds }
+        })
+        
+    }
+
+    useEffect(()=> {
+        if(data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session })
+            });
+        }    
+    }, [data])
 
     if (!state.cartOpen) {
         return (
@@ -54,7 +84,7 @@ const Cart = () => {
                         <strong>Total: ${calculateTotal()}</strong>
                         {
                             Auth.loggedIn() ?
-                            <button>
+                            <button onClick={submitCheckout}>
                                 Checkout
                             </button>
                             :
